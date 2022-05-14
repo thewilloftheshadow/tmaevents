9d8ea105c47b7b4208f444e37da341aefdac0b28
Shadow <william.shadow@outlook.com>
2022-05-14 16:12:51 -0500

    weeklycount command

---
+++ commands/events/weeklycount.js
@@ -0,0 +1,28 @@
+const { ids } = require("../../config")
+
+module.exports = {
+  command: {
+    name: "weeklycount",
+    description: "Count the number of users on each weekly stage",
+    defaultPermission: true,
+  },
+  permissions: [],
+  run: async (interaction, client) => {
+    if(interaction.guild.id != ids.tma) return interaction.reply({content: "This command can only be used in the main TMA server!", ephemeral: true})
+
+    await interaction.deferReply()
+    
+    let wr = ids.weekly
+
+    let message = ""
+
+    await Promise.all(
+        wr.map(async id => {
+            let role = await message.guild.roles.resolve(id)
+            message += `<@&${id}>: ${role?.members?.cache.size || 0} players`
+        })
+    )
+
+    interaction.reply({content: message, allowedMentions: {roles: []}})
+  },
+}
